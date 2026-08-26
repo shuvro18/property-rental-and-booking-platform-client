20dd3e3628ac8309ec3b3fb3b42d22407f672300
shuvro18 <shuvrobiswas404@gmail.com>
2026-08-26 08:27:01 +0600

    image optimization for google account photo added

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,12 @@ const nextConfig = {
         hostname: 'practicaltyping.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
       }
     ]
   }
