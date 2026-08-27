fcc95a5dafcde2ed407c20e831acf6e0232d036f
shuvro18 <shuvrobiswas404@gmail.com>
2026-08-27 13:52:43 +0600

    proxy page added

---
+++ src/app/properties/[id]/page.jsx
@@ -0,0 +1,13 @@
+
+
+const propertyDetailPage = async({params}) => {
+    const {id} = await params
+    console.log(id)
+    return (
+        
+
+        
+    );
+};
+
+export default propertyDetailPage;
---
+++ src/proxy.js
@@ -0,0 +1,30 @@
+
+import { auth } from './app/lib/auth'
+import { headers } from 'next/headers'
+import { NextResponse } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export async function proxy(request) {
+    const url = request.nextUrl;
+
+    const session = await auth.api.getSession({
+    headers: await headers()
+   })
+
+   if(url.pathname === "/properties"){
+    return NextResponse.next()
+   }
+
+   if(!session){
+    return NextResponse.redirect(new URL('/register', request.url))
+   }
+
+//    return NextResponse.next()
+}
+ 
+// Alternatively, you can use a default export:
+// export default function proxy(request) { ... }
+ 
+export const config = {
+  matcher: ['/properties/:path*'],
+}
