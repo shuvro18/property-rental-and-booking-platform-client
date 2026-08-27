7159b937c077df110e8c28c93d4f80a22fc0c7be
shuvro18 <shuvrobiswas404@gmail.com>
2026-08-27 14:16:36 +0600

    api for details house added

--- src/app/lib/data.js
+++ src/app/lib/data.js
@@ -1,7 +1,15 @@
 const backEndUrl = process.env.NEXT_PUBLIC_BACKEND_URL
 
+// all houses
 export const getHouses = async() =>{
     const res = await fetch(`${backEndUrl}/houses`);
+    const data = await res.json();
+    return data
+}
+
+// single houser 
+export const getSingleHouse = async(id) => {
+    const res = await fetch(`${backEndUrl}/houses/${id}`);
     const data = await res.json();
     return data
 }
