ba73bfecefef62fb34093f2e0182ec55fc95635e
shuvro18 <shuvrobiswas404@gmail.com>
2026-08-29 21:18:08 +0600

    owner profile page added

---
+++ src/app/dashboard/owner/profile/page.jsx
@@ -0,0 +1,12 @@
+import ProfilePage from '@/app/components/dashboard/Profile';
+import React from 'react';
+
+const ownerProfilePage = () => {
+    return (
+        <div>
+            <ProfilePage/>
+        </div>
+    );
+};
+
+export default ownerProfilePage;
